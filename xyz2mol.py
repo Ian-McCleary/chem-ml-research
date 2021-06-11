0911bc8a87cca7e2eb441481122c5e87a1b5c06a
Ian-McCleray <mccleai@wwu.edu>
2021-06-11 14:03:12 -0700

    Added Deepchem Coulomb Matrix Support

--- xyz2mol.py
+++ xyz2mol.py
@@ -820,6 +820,6 @@ if __name__ == "__main__":
             generator = dc.utils.ConformerGenerator(max_conformers=5)
             conformer_mol = generator.generate_conformers(mol)
             print("Number of available conformers for this molecule: ", len(conformer_mol.GetConformers()))
-            coulomb_mat = dc.feat.CoulombMatrix(max_atoms=50)
+            coulomb_mat = dc.feat.CoulombMatrix(max_atoms=60)
             features = coulomb_mat(conformer_mol)
             print(features)
