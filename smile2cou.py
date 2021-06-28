232ef09a7f1dc7d2077e0cf13a12ea6c45a8e9e7
Ian-McCleray <mccleai@wwu.edu>
2021-06-28 15:18:47 -0700

    testing for coulomb from smiles

--- smile2cou.py
+++ smile2cou.py
@@ -7,6 +7,6 @@ generator = dc.utils.ConformerGenerator(max_conformers=5)
 azo_mol = generator.generate_conformers(Chem.MolFromSmiles(smile1))
 print("Number of available conformers for propane: ", len(azo_mol.GetConformers()))
 
-coulomb_mat = dc.feat.CoulombMatrix(max_atoms=20)
+coulomb_mat = dc.feat.CoulombMatrix(max_atoms=50)
 features = coulomb_mat(azo_mol)
 print(features)
