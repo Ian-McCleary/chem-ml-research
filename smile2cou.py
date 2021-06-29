947e73beb83fef934877e84f4c1251623c9207f9
Ian-McCleray <mccleai@wwu.edu>
2021-06-29 12:06:18 -0700

    Updated dataset creation

--- smile2cou.py
+++ smile2cou.py
@@ -17,7 +17,7 @@ m_980818 = 'COc1ccc(\\N=N/c2cc(F)ccc2C(=O)O)c(OC)c1C#N'
 m_983269 = 'COc1c(F)cccc1\\N=N/c1c(F)cccc1C(=O)O'
 m_998710 = 'Cc1cc(C(=O)O)cc(\\N=N/c2cc(F)cc(C(=O)O)c2)c1C'
 m_1004663 = 'N#Cc1ccc(C(=O)O)c(C#N)c1\\N=N/c1cc(C(=O)O)ccc1F'
-smile_arr = [m_51677,m_168242,m_857149,m_945444,m_963853,m_971449,m_980818,m_983269,m_998710,m_1004663]
+smile_arr = [m_51677, m_168242, m_857149, m_945444, m_963853, m_971449, m_980818, m_983269, m_998710, m_1004663]
 
 # Energy difference calculated from NEB.out. Is this ground state difference?
 # -metastable - (-stable)
@@ -31,10 +31,10 @@ y_980818 = [-1539.706923+1539.912726]
 y_983269 = [-1389.671748+1389.934824]
 y_998710 = [-1486.485683+1486.563960]
 y_1004663 = [-1554.285506+1554.414150]
-output_y = np.array([y_51677,y_168242,y_857149,y_945444,y_963853,y_971449,y_980818,y_983269,y_998710,y_1004663])
+output_y = np.array([y_51677, y_168242, y_857149, y_945444, y_963853, y_971449, y_980818, y_983269, y_998710, y_1004663])
 
-input_X = np.zeros([10,45,45])
-id_arr = np.array([51677,168242,857149,945444,963853,971449,980818,983269,998710,1004663])
+input_X = np.zeros([10, 45, 45])
+id_arr = np.array([51677, 168242, 857149, 945444, 963853, 971449, 980818, 983269, 998710, 1004663])
 
 # Generate Coulomb matrix with 2 conforms. Should be a 2d vector
 for i in range(len(smile_arr)):
@@ -51,11 +51,13 @@ for i in range(len(smile_arr)):
 #    print(output_y[x])
 #    print("\n")
 
-data = dc.data.NumpyDataset(X=input_X,y=output_y,ids=id_arr, n_tasks=1)
+data = dc.data.NumpyDataset(X=input_X, y=output_y, ids=id_arr, n_tasks=1)
 # Splits dataset into train/validation/test
 splitter = dc.splits.RandomSplitter()
 train_dataset, valid_dataset, test_dataset = splitter.train_valid_test_split(dataset=data)
 print(len(train_dataset))
+
+metric = dc.metrics.Metric(dc.metrics.mean_absolute_error, mode="regression")
 
 model = dc.models.DTNNModel(
     n_tasks=1,
@@ -63,6 +65,13 @@ model = dc.models.DTNNModel(
     mode="regression",
     dropout=0.5
 )
+model.fit(train_dataset)
+
+train_scores = model.evaluate(train_dataset, metric)
+valid_score = model.evaluate(valid_dataset, metric)
+test_score = model.evaluate(test_dataset, metric)
+print(train_scores + ", " + valid_score + ", " + test_score)
+
 
 
 #Generate Coulomb matrix
