90e1c8dac29e50afbdec659943f5e4fb8de010d4
Ian-McCleray <mccleai@wwu.edu>
2021-06-28 17:03:14 -0700

    Added sample dataset

--- smile2cou.py
+++ smile2cou.py
@@ -1,12 +1,25 @@
 from rdkit import Chem
 import deepchem as dc
 
-smile1 = "COc1cc(C#N)c(\\N=N/c2ccccc2C(=O)O)c(C(=O)O)c1"
+#Meta smiles strings randomly sampled from Batch 4
+smile1 = 'COc1cc(C#N)c(\\N=N/c2ccccc2C(=O)O)c(C(=O)O)c1'
+smile2 = 'Cc1ccc(-c2ccccc2)c(-c2ccccc2)c1\\N=N/c1ccccc1F'
+smile3 = 'Cc1cc(C(=O)O)cc(\\N=N/c2cc(F)cc(C(=O)O)c2)c1C'
+smile4 = 'N#Cc1ccc(C(=O)O)c(C#N)c1\\N=N/c1cc(C(=O)O)ccc1F'
 
-generator = dc.utils.ConformerGenerator(max_conformers=5)
-azo_mol = generator.generate_conformers(Chem.MolFromSmiles(smile1))
-print("Number of available conformers for propane: ", len(azo_mol.GetConformers()))
 
-coulomb_mat = dc.feat.CoulombMatrix(max_atoms=50)
-features = coulomb_mat(azo_mol)
-print(features)
+tasks, datasets, transformers = dc.molnet.load_delaney(featurizer='GraphConv')
+train_dataset, valid_dataset, test_dataset = datasets
+
+print(test_dataset)
+
+
+
+#Generate Coulomb matrix
+#generator = dc.utils.ConformerGenerator(max_conformers=5)
+#azo_mol = generator.generate_conformers(Chem.MolFromSmiles(smile1))
+#print("Number of available conformers for propane: ", len(azo_mol.GetConformers()))
+#coulomb_mat = dc.feat.CoulombMatrix(max_atoms=50)
+#features = coulomb_mat(azo_mol)
+#print(features)
+
