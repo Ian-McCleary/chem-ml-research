9910a323dfe5e32ad91cc88bee5085530da03c15
Ian-McCleray <mccleai@wwu.edu>
2021-06-30 16:15:04 -0700

    Implemented dataset creator

--- ds_creator.py
+++ ds_creator.py
@@ -1,37 +1,46 @@
 import argparse
 import os
 
+#currently named Azo_Data_1 on cluster
 start_dir = "./Data/"
 
-directory_list = os.listdir(start_dir)
-for batch in directory_list:
-    batch_path = os.path.join(start_dir, batch)
-    mol_list = os.listdir(batch_path)
-    for mol in mol_list:
-        mol_path = os.path.join(batch_path, mol)
-        if os.path.isdir(mol_path):
-            #Create the path for smile string. Must use os.path.join() and cannot use string + operator
-            sstr1 = "Meta"
-            s_append1 = os.path.join(mol_path, sstr1)
-            s_filename = mol + "_Meta.smiles"
-            #smile_append_path = os.path.normpath(smile_append_str)
-            smile_rel_path = os.path.join(s_append1, s_filename)
-            smile_absolute = os.path.realpath(smile_rel_path)
-            print(smile_rel_path)
-            if os.path.isfile(smile_rel_path):
-                print("found!")
-                f = open(os.path.realpath(smile_rel_path), "r")
-                print(f.readline())
+start_ds_creation()
+
+# TODO Add parameters which allow specific data collection fields and create customizable featurizer function
+def start_ds_creation():
+    directory_list = os.listdir(start_dir)
+    for batch in directory_list:
+        batch_path = os.path.join(start_dir, batch)
+        mol_list = os.listdir(batch_path)
+        for mol in mol_list:
+            mol_path = os.path.join(batch_path, mol)
+            if os.path.isdir(mol_path):
+                get_smiles(mol_path, mol)
             else:
-                print("not a smile path")
-        else:
-            print("not a mol_path")
+                print("Invalid molecule filepath" + mol_path)
 
 
-            #rel_path1 = os.path.relpath(os.path.join(root, subdirectory))
-            #rel_path2 = os.path.join(rel_path1,file)
-            #name, extension = os.path.splitext(rel_path2)
-            #if "smiles" in extension:
-             #   print("found!")
-                #f = open(os.path.realpath(rel_path2), "r")
-                #print(f.readline())
+
+def get_smiles(rel_molecule_path, molecule_name):
+    # Create the path for smile string. Must use os.path.join() and cannot use string + operator
+    # Using Meta smile string
+    sstr1 = "Meta"
+    s_append1 = os.path.join(rel_molecule_path, sstr1)
+    s_filename = molecule_name + "_Meta.smiles"
+    # smile_append_path = os.path.normpath(smile_append_str)
+    smile_rel_path = os.path.join(s_append1, s_filename)
+    smile_absolute = os.path.realpath(smile_rel_path)
+    print(smile_rel_path)
+    if os.path.isfile(smile_rel_path):
+        f = open(os.path.realpath(smile_rel_path), "r")
+        print(f.readline())
+    else:
+        print("Invalid smiles filepath")
+
+
+def main():
+    start_ds_creation()
+
+if __name__ == "__main__":
+    main()
+
