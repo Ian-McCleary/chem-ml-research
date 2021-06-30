32798505d478805fe4996d6706ccf09af4969562
Ian-McCleray <mccleai@wwu.edu>
2021-06-30 16:46:05 -0700

    Implemented dataset creator

--- ds_creator.py
+++ ds_creator.py
@@ -1,22 +1,43 @@
 import argparse
 import os
 
-#currently named Azo_Data_1 on cluster
+# currently named Azo_Data_1 on cluster
 start_dir = "./Data/"
 
-# TODO Add parameters which allow specific data collection fields and create customizable featurizer function
+
+# TODO Add parameters which allow specific data collection fields. Remove print statements.
 def start_ds_creation():
     directory_list = os.listdir(start_dir)
     for batch in directory_list:
         batch_path = os.path.join(start_dir, batch)
         mol_list = os.listdir(batch_path)
-        for mol in mol_list:
-            mol_path = os.path.join(batch_path, mol)
+        for molecule in mol_list:
+            mol_path = os.path.join(batch_path, molecule)
             if os.path.isdir(mol_path):
-                get_smiles(mol_path, mol)
+                get_smiles(mol_path, molecule)
+                neb_path = get_neb_path(mol_path)
+                get_barrier_height(neb_path)
+                print("\n")
             else:
                 print("Invalid molecule filepath" + mol_path)
 
+
+# Create the path for neb_out string. Must use os.path.join() and cannot use string + operator
+def get_neb_path(rel_molecule_path):
+    neb1 = "neb"
+    neb2 = "neb_out"
+    rel_neb = os.path.join(rel_molecule_path, neb1)
+    rel_neb_out = os.path.join(rel_neb, neb2)
+    return rel_neb_out
+
+
+def get_barrier_height(neb_path):
+    b_file = "barrier.height"
+    barrier_path = os.path.join(neb_path, b_file)
+    if os.path.isfile(barrier_path):
+        f = open(os.path.realpath(barrier_path), "r")
+        print(f.readline())
+        return f.readline()
 
 
 def get_smiles(rel_molecule_path, molecule_name):
@@ -25,7 +46,6 @@ def get_smiles(rel_molecule_path, molecule_name):
     sstr1 = "Meta"
     s_append1 = os.path.join(rel_molecule_path, sstr1)
     s_filename = molecule_name + "_Meta.smiles"
-    # smile_append_path = os.path.normpath(smile_append_str)
     smile_rel_path = os.path.join(s_append1, s_filename)
     smile_absolute = os.path.realpath(smile_rel_path)
     print(smile_rel_path)
@@ -35,10 +55,16 @@ def get_smiles(rel_molecule_path, molecule_name):
     else:
         print("Invalid smiles filepath")
 
+# Allows for custom featurizer code for specific models
+def featurize_smiles(smile_string):
+    # add deepchem featurizer code here such as Coulombmatrix ect..
+    feature = smile_string
+    return feature
+
 
 def main():
     start_ds_creation()
 
+
 if __name__ == "__main__":
     main()
-
