17632fd60548aaa288fa16af3e866f2d912a4885
Ian-McCleray <mccleai@wwu.edu>
2021-06-30 15:58:53 -0700

    Implemented dataset creator

--- ds_creator.py
+++ ds_creator.py
@@ -10,10 +10,12 @@ for batch in directory_list:
     for mol in mol_list:
         mol_path = os.path.join(batch_path, mol)
         if os.path.isdir(mol_path):
-            #print(mol_path)
-            smile_append_str = "/Meta/" + mol + "_Meta.smiles"
-            smile_append_path = os.path.normpath(smile_append_str)
-            smile_rel_path = os.path.join(mol_path, smile_append_path)
+            #Create the path for smile string. Must use os.path.join() and cannot use string + operator
+            sstr1 = "Meta"
+            s_append1 = os.path.join(mol_path, sstr1)
+            s_filename = mol + "_meta.smiles"
+            #smile_append_path = os.path.normpath(smile_append_str)
+            smile_rel_path = os.path.join(s_append1, s_filename)
             smile_absolute = os.path.realpath(smile_rel_path)
             print(smile_rel_path)
             if os.path.isfile(smile_absolute):
