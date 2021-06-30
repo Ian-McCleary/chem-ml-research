b01b2b713ae2e75bed6d71963d66f6b6723f2ccf
Ian-McCleray <mccleai@wwu.edu>
2021-06-30 14:35:14 -0700

    Implemented dataset creator

--- ds_creator.py
+++ ds_creator.py
@@ -5,12 +5,16 @@ start_dir = "./Data/"
 
 for root, subdirectories, files in os.walk(start_dir):
     for subdirectory in subdirectories:
+        directory_path = os.path.join(root,subdirectory)
+        batch,folder_name = os.path.splitext(directory_path)
+        print(batch)
+        print(folder_name)
         for file in files:
             rel_path1 = os.path.relpath(os.path.join(root, subdirectory))
             rel_path2 = os.path.join(rel_path1,file)
             print(rel_path2)
             name, extension = os.path.splitext(rel_path2)
             if "smiles" in extension:
-                print("found!")
-                f = open(os.path.realpath(rel_path2), "r")
-                print(f.readline())
+                #print("found!")
+                #f = open(os.path.realpath(rel_path2), "r")
+                #print(f.readline())
