d642dca5f6ea31fc7b016dc0fb955dd9469cfec8
Ian-McCleray <mccleai@wwu.edu>
2021-06-30 13:34:58 -0700

    Implemented DTNN Model

--- ds_creator.py
+++ ds_creator.py
@@ -7,6 +7,7 @@ for root, subdirectories, files in os.walk(start_dir):
     for subdirectory in subdirectories:
         for file in files:
             rel_path = os.path.relpath(os.path.join(root, subdirectory))
+            print(rel_path)
             name, extension = os.path.splitext(rel_path)
             if "smiles" in extension:
                 print("found!")
