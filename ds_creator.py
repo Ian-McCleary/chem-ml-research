fa326b4fc041552c91def133dc6d0df5eaf727d4
Ian-McCleray <mccleai@wwu.edu>
2021-07-01 17:10:20 -0700

    Implemented dataset creator

--- ds_creator.py
+++ ds_creator.py
@@ -90,7 +90,7 @@ def featurize_smiles(smile_arr):
 def create_save_dataset(id, input, output):
     dataset = dc.data.NumpyDataset(X=input, y=output, ids=id)
     file_name = "dataset_out"
-    dc.data.to_json(dataset, file_name)
+    dataset.to_json(dataset, file_name)
     print("DONE!")
 
 
