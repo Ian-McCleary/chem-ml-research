0cce6a26acac013c51e5fb6a1aac7b9679b20e0c
Ian-McCleray <mccleai@wwu.edu>
2021-07-02 15:06:18 -0700

    Testing

--- DTNN_Model.py
+++ DTNN_Model.py
@@ -79,7 +79,7 @@ print(valid_score)
 print("Test Scores: ")
 print(test_score)
 
-#generated_batch = dc.models.DTNNModel.default_generator(self=model, dataset=data, epochs=2, mode='fit', deterministic=True, pad_batches=True)
+generated_batch = dc.models.DTNNModel.default_generator(self=model, dataset=data, epochs=5, mode='fit', deterministic=False, pad_batches=True)
 compute_features = dc.models.DTNNModel.compute_features_on_batch(self=model, X_b=data.X)
 print(compute_features)
 
