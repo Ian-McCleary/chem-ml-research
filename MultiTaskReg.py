9647c70f1b44d7e6a5d5efc2b29cd8b01ffea4e5
Ian-McCleray <mccleai@wwu.edu>
2021-07-06 13:28:20 -0700

    Testing hyperparameter optimization & Circular Fingerprint

--- MultiTaskReg.py
+++ MultiTaskReg.py
@@ -3,11 +3,16 @@ import deepchem as dc
 import numpy as np
 
 data = dc.data.datasets.NumpyDataset.from_json("dataset_out")
+transformer = dc.trans.NormalizationTransformer(transform_y=True, dataset=data)
+dataset = transformer.transform(data)
+
 # Splits dataset into train/validation/test
 splitter = dc.splits.RandomSplitter()
-train_dataset, valid_dataset, test_dataset = splitter.train_valid_test_split(dataset=data)
-task_count = len(data.y[0])
-n_features = len(data.X[0])
+train_dataset, valid_dataset, test_dataset = splitter.train_valid_test_split(dataset=dataset)
+task_count = len(dataset.y[0])
+n_features = len(dataset.X[0])
+
+
 
 params_dict = {
     'n_tasks': [task_count],
@@ -20,5 +25,5 @@ print(data.y)
 optimizer = dc.hyper.GridHyperparamOpt(dc.models.MultitaskClassifier)
 metric = dc.metrics.Metric(dc.metrics.roc_auc_score)
 best_model, best_hyperparams, all_results = optimizer.hyperparam_search(
-        params_dict, train_dataset, valid_dataset, metric, data.y)
+        params_dict, train_dataset, valid_dataset, metric, transformer)
 print(all_results)
