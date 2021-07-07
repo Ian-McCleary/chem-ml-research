788f0880b34d3360e6697e2e770b9761b427abf3
ian <awsomer365@gmail.com>
2021-07-07 15:34:42 -0700

    testing

--- DTNN_Model.py
+++ DTNN_Model.py
@@ -12,10 +12,10 @@ task_count = len(data.y[0])
 
 
 metrics = [
-    dc.metrics.Metric(dc.metrics.mean_squared_error),
+    dc.metrics.Metric(dc.metrics.rms_score),
     dc.metrics.Metric(dc.metrics.r2_score)
     ]
-metric = dc.metrics.Metric(dc.mean_squared_error)
+metric = dc.metrics.Metric(dc.rms_score)
 
 # parameter optimization
 params_dict = {
