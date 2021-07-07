fbeebb695f0897103901ace856a4254b0e1c014a
awsomer365 <mccleai@wwu.edu>
2021-07-07 09:38:37 -0700

    testing hyperparameters

--- MultiTaskReg.py
+++ MultiTaskReg.py
@@ -2,30 +2,33 @@ from rdkit import Chem
 import deepchem as dc
 import numpy as np
 
-data = dc.data.datasets.NumpyDataset.from_json("dataset_out")
-transformer = dc.trans.BalancingTransformer(dataset=data)
-dataset = transformer.transform(data)
+#data = dc.data.datasets.NumpyDataset.from_json("dataset_out")
+#transformer = dc.trans.BalancingTransformer(dataset=data)
+#dataset = transformer.transform(data)
 
 # Splits dataset into train/validation/test
-splitter = dc.splits.RandomSplitter()
-train_dataset, valid_dataset, test_dataset = splitter.train_valid_test_split(dataset=dataset)
-task_count = len(dataset.y[0])
-n_features = len(dataset.X[0])
+#splitter = dc.splits.RandomSplitter()
+#train_dataset, valid_dataset, test_dataset = splitter.train_valid_test_split(dataset=dataset)
+#task_count = len(dataset.y[0])
+#n_features = len(dataset.X[0])
+
+tasks, datasets, transformers = dc.molnet.load_hiv(featurizer='ECFP', split='scaffold')
+train_dataset, valid_dataset, test_dataset = datasets
 
 
 
 params_dict = {
-    'n_tasks': [task_count],
-    'n_features': [n_features],
+    'n_tasks': [len(tasks)],
+    'n_features': [1024],
     'layer_sizes': [[500], [1000], [1000, 1000]],
     'dropouts': [0.2, 0.5],
     'learning_rate': [0.001, 0.0001]
 }
-print(data.y)
+#print(data.y)
 
 
 optimizer = dc.hyper.GridHyperparamOpt(dc.models.MultitaskClassifier)
-metric = dc.metrics.Metric(dc.metrics.mean_absolute_error, mode="regression")
+metric = dc.metrics.Metric(dc.metrics.roc_auc_score)
 best_model, best_hyperparams, all_results = optimizer.hyperparam_search(
         params_dict, train_dataset, valid_dataset, metric, transformer)
 print(all_results)
