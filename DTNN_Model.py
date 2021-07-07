8c4d9365a7f8566ceb428490e128655cc77dff01
ian <awsomer365@gmail.com>
2021-07-07 13:19:40 -0700

    testing

--- DTNN_Model.py
+++ DTNN_Model.py
@@ -12,7 +12,8 @@ task_count = len(data.y[0])
 
 
 metric = dc.metrics.Metric(dc.metrics.mean_absolute_error, mode="regression")
-
+# parameter optimization
+'''
 params_dict = {
     'n_tasks': [task_count],
     'n_embedding': [10, 40, 100, 1000],
@@ -23,21 +24,21 @@ params_dict = {
 optimizer = dc.hyper.GridHyperparamOpt(dc.models.DTNNModel)
 transformers = [dc.trans.NormalizationTransformer(transform_y=True, dataset=data)]
 best_model, best_hyperparams, all_results =  optimizer.hyperparam_search(params_dict, train_dataset, valid_dataset,
-                                                                         metric, transformers)
+                                                                         metric, transformers)                                                                  
+print(all_results) '''
 
-                                                                    
-print(all_results)
+# Single evaluation model
 
-''' model = dc.models.DTNNModel(
+model = dc.models.DTNNModel(
     n_tasks=task_count,
-    n_embedding=30,
+    n_embedding=1000,
     mode="regression",
-    dropout=0.1,
-    learning_rate=0.001
+    dropout=0.5,
+    learning_rate=0.0001
 )
-
-#print(all_results)
-
+callback = dc.models.ValidationCallback(valid_dataset, 1000, metric)
+model.fit(train_dataset, nb_epoch=50, callbacks=callback)
+'''
 model.fit(train_dataset)
 # How well the model fit's the training subset of our data
 train_scores = model.evaluate(train_dataset, metric)
@@ -50,6 +51,6 @@ print(train_scores)
 print("Validity Scores: ")
 print(valid_score)
 print("Test Scores: ")
-print(test_score) '''
-
+print(test_score)
+'''
 
