28286b3e6a85084495c5dcd879a8601d8ddef3d9
ian <awsomer365@gmail.com>
2021-07-07 16:46:17 -0700

    testing

--- DTNN_Model.py
+++ DTNN_Model.py
@@ -38,7 +38,7 @@ print(best_hyperparams)
 # Single evaluation model
 # Single task: params (1, 50, 0.2, 1e-06)
 '''
-print(data.getshape())
+print(data.get_shape())
 model = dc.models.DTNNModel(
     n_tasks=task_count,
     n_embedding=50,
