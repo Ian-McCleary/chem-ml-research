64818af2b74aad438e1897cf9d8c8a458c5a9884
ian <awsomer365@gmail.com>
2021-07-07 16:48:33 -0700

    testing

--- DTNN_Model.py
+++ DTNN_Model.py
@@ -24,7 +24,7 @@ params_dict = {
     'dropouts': [0.1, 0.2, 0.5, 0.9],
     'learning_rate': [0.001, 0.0001, 0.00001, 0.000001]
 }
-'''
+
 optimizer = dc.hyper.GridHyperparamOpt(dc.models.DTNNModel)
 transformers = [dc.trans.NormalizationTransformer(transform_y=True, dataset=data)]
 best_model, best_hyperparams, all_results =  optimizer.hyperparam_search(params_dict, train_dataset, valid_dataset,
@@ -61,3 +61,4 @@ print("Validity Scores: ")
 print(valid_score)
 print("Test Scores: ")
 print(test_score)
+'''
