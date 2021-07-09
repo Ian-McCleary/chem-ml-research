959824d48f22c7992083a4a474e54d580fc222a8
ian <mccleai@wwu.edu>
2021-07-09 12:57:57 -0700

    readded RDkit import

--- GraphConvMol.py
+++ GraphConvMol.py
@@ -1,3 +1,4 @@
+from rdkit import Chem
 import deepchem as dc
 import numpy as np
 import pandas as pd
--- example_loss.py
+++ example_loss.py
@@ -1,3 +1,4 @@
+from rdkit import Chem
 import deepchem as dc
 import numpy as np
 import matplotlib.pyplot as plt
