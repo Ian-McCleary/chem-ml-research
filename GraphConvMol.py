5d0492efb48a1d265ecc55e7bc283b55e2a57308
ian <mccleai@wwu.edu>
2021-07-08 17:31:33 -0700

    testing

--- GraphConvMol.py
+++ GraphConvMol.py
@@ -8,7 +8,7 @@ tf.random.set_seed(123)
 np.random.seed(123)
 # update task count as list ["task1", "task2"..]
 loader = dc.data.CSVLoader(["task1"], feature_field="smiles", id_field="ids", featurizer=dc.feat.ConvMolFeaturizer(per_atom_fragmentation=False))
-data = loader.create_dataset("dataset_out.csv")
+data = loader.create_dataset("dataset_10.csv")
 transformer = dc.trans.NormalizationTransformer(transform_y=True, dataset=data)
 dataset = transformer.transform(data)
 
@@ -20,11 +20,41 @@ task_count = len(train_dataset.y[0])
 # metric = dc.metrics.Metric(dc.metrics.pearson_r2_score, np.mean)
 metric = dc.metrics.Metric(dc.metrics.rms_score)
 
+# parameter optimization
+params_dict = {
+    'n_tasks': [task_count],
+    'number_atom_features': [15, 30, 75, 100, 150],
+    'graph_conv_layers': [[32, 32], [64,64], [128,128]],
+    'dense_layer_size': [8, 16, 32, 64, 128],
+    'dropouts': [0.1, 0.2, 0.5, 0.9],
+    'learning_rate': [0.001, 0.0001, 0.00001, 0.000001, 0.0000001]
+}
+
+optimizer = dc.hyper.GridHyperparamOpt(dc.models.DTNNModel)
+transformers = [dc.trans.NormalizationTransformer(transform_y=True, dataset=data)]
+best_model, best_hyperparams, all_results =  optimizer.hyperparam_search(params_dict, train_dataset, valid_dataset,
+                                                                         metric, transformers)                                                                  
+print(all_results)
+print("\n")
+print(best_model)
+print("\n")
+print(best_hyperparams)
+
+# Single evaluation model
+# Single task: params (1, 50, 0.2, 1e-06)
+print("Hyperparam list")
+print(best_hyperparams[1])
+print(best_hyperparams[2])
+
+
 # supports width of channels from convolutional layers and dropout for each layer.
 model = dc.models.GraphConvModel(
   n_tasks=task_count,
-  number_atom_features=75,
-  batch_size=128,
+  number_atom_features=best_hyperparams[1],
+  graph_conv_layers=best_hyperparams[2],
+  dense_layer_size=best_hyperparams[3],
+  dropouts=best_hyperparams[4],
+  learning_rate=best_hyperparams[5],
   mode="regression"
 )
 
@@ -33,6 +63,14 @@ model = dc.models.GraphConvModel(
 losses = []
 for i in range(20):
   loss = model.fit(train_dataset, nb_epoch=1)
+  print("loss: %s" % str(loss))
+  losses.append(loss)
+print("losses")
+print(losses)
+
+losses = []
+for i in range(20):
+  loss = model.fit(valid_dataset, nb_epoch=1)
   print("loss: %s" % str(loss))
   losses.append(loss)
 print("losses")
