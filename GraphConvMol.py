7fee4714f810147f7356fc7b0cc05e22a8703fb1
ian <mccleai@wwu.edu>
2021-07-09 08:01:10 -0700

    testing

--- GraphConvMol.py
+++ GraphConvMol.py
@@ -67,6 +67,7 @@ model = dc.models.GraphConvModel(
   dense_layer_size=best_hyperparams[3],
   dropouts=best_hyperparams[4],
   learning_rate=best_hyperparams[5],
+  batch_normalize=False,
   mode="regression"
 )
 '''
