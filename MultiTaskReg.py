75adefd83d0a8d125388dcab6b7e9befa4b764bd
ian <mccleai@wwu.edu>
2021-07-14 16:21:19 -0700

    updated

--- MultiTaskReg.py
+++ MultiTaskReg.py
@@ -1,9 +1,11 @@
 from rdkit import Chem
 import deepchem as dc
 import numpy as np
+import pandas as pd
 
-data = dc.data.datasets.NumpyDataset.from_json("dataset_out")
-transformer = dc.trans.BalancingTransformer(dataset=data)
+loader = dc.data.CSVLoader(["task1"], feature_field="smiles", id_field="ids", featurizer=dc.feat.CircularFingerprint(size=2048, radius=2))
+data = loader.create_dataset("Datasets/dataset_100.csv")
+transformer = dc.trans.NormalizationTransformer(dataset=data)
 dataset = transformer.transform(data)
 
 # Splits dataset into train/validation/test
@@ -16,12 +18,10 @@ print(task_count)
 #tasks, datasets, transformers = dc.molnet.load_hiv(featurizer='ECFP', split='scaffold')
 #train_dataset, valid_dataset, test_dataset = datasets
 
-
-
 params_dict = {
     'n_tasks': [task_count],
     'n_features': [n_features],
-    'layer_sizes': [[500], [1000], [1000, 1000]],
+    'layer_sizes': [[500], [1000]],
     'dropouts': [0.2, 0.5],
     'learning_rate': [0.001, 0.0001],
     'n_classes': [len(data.y)]
@@ -29,8 +29,41 @@ params_dict = {
 #print(data.y)
 
 
-optimizer = dc.hyper.GridHyperparamOpt(dc.models.MultitaskClassifier)
-metric = dc.metrics.Metric(dc.metrics.roc_auc_score)
+optimizer = dc.hyper.GridHyperparamOpt(dc.models.MultitaskRegressor)
+metric = dc.metrics.Metric(dc.metrics.rms_score)
 best_model, best_hyperparams, all_results = optimizer.hyperparam_search(
         params_dict, train_dataset, valid_dataset, metric, transformer)
 print(all_results)
+
+model = dc.models.MultitaskRegressor(
+    n_tasks=task_count,
+    n_features=n_features,
+    layer_sizes=best_hyperparams[2],
+    dropouts=best_hyperparams[3],
+    learning_rate=best_hyperparams[4],
+    mode="regression"
+  )
+
+
+# Fit trained model
+# test
+train_losses = []
+for i in range(100):
+  loss = model.fit(train_dataset, nb_epoch=1)
+  print("loss: %s" % str(loss))
+  train_losses.append(loss)
+print("losses")
+print(train_losses)
+print("\n")
+print("Valid_dataset losses:")
+
+valid_losses = []
+for i in range(100):
+    loss = model.fit(valid_dataset, nb_epoch=1)
+    print("loss: %s" % str(loss))
+    valid_losses.append(loss)
+print("losses")
+print(valid_losses)
+
+df = pd.DataFrame(list(zip(train_losses, valid_losses)), columns=["train_losses", "valid_losses"])
+df.to_csv("mtr_losses.csv")
