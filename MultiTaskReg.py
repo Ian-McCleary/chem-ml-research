d8a451c39cea0c18dd8875b981c92d9db192c686
ian <mccleai@wwu.edu>
2021-07-15 14:11:47 -0700

    bug fix

--- MultiTaskReg.py
+++ MultiTaskReg.py
@@ -87,11 +87,17 @@ for x in range(3):
   valid_score = model.evaluate(valid_dataset, metric)
   # How well the model generalizes the rest of the data
   test_score = model.evaluate(test_dataset, metric)
-
+  train = []
+  valid = []
+  test = []
+  train.append(train_scores)
+  valid.append(valid_score)
+  test.append(test_score)
+  for p in range(299):
+    train.append(0)
+    valid.append(0)
+    test.append(0)
 
   file_name = "mtr_losses_" + str(x+1)+ "task_default.csv"
-  df = pd.DataFrame(list(zip(train_losses, valid_losses)), columns=["train_losses", "valid_losses"])
-  df.insert(4,"train_score", train_scores)
-  df.insert(5,"valid_score", valid_score)
-  df.insert(6,"test_score", test_score)
+  df = pd.DataFrame(list(zip(train_losses, valid_losses, train, valid, test)), columns=["train_losses", "valid_losses", "train_score", "valid_score", "test_score"])
   df.to_csv(file_name)
