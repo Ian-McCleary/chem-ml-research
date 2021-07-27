12877fbd5b0f77e7ad2f56801a731e0c5941c1cc
mccleai <mccleai@wwu.edu>
2021-07-27 16:43:25 -0700

    testing 32, 64, 128 layer sizes

--- MultiTaskReg.py
+++ MultiTaskReg.py
@@ -51,7 +51,7 @@ def start_training():
     all_loss = train_loss(model, train_dataset, valid_dataset, metric, [transformer])
     print("csv: ")
     # hyperparameter_optimization()
-    file_name = "mtr_3task_hyperparam.csv"
+    file_name = "mtr_3task_hyperparam_2048.csv"
     df = pd.DataFrame(list(zip(all_loss[0], all_loss[1], all_loss[2], all_loss[3], all_loss[4], all_loss[5], all_loss[6], all_loss[7])), columns=[
         "train_mean", "train_eiso", "train_riso", "train_vert", "valid_mean", "valid_eiso", "valid_riso", "valid_vert"])
 
@@ -153,7 +153,7 @@ def train_loss(model, train_dataset, valid_dataset, metric, transformer):
     valid_vert = []
     all_loss = []
 
-    for i in range(750):
+    for i in range(500):
         loss = model.fit(train_dataset, nb_epoch=1)
         train = model.evaluate(train_dataset, metric, transformer, per_task_metrics=True)
         valid = model.evaluate(valid_dataset, metric, transformer, per_task_metrics=True)
