7cd328ba180e1409fa7b7866f41b076ca837eb01
ian <mccleai@wwu.edu>
2021-07-27 10:57:46 -0700

    updated file structure

--- DTNN_Model.py
+++ DTNN_Model.py
@@ -20,65 +20,26 @@ tf.compat.v1.keras.backend.set_session(session)
 dataseed = randrange(1000)
 np.random.seed(dataseed)
 tf.random.set_seed(dataseed)
+
 def start_training():
 
-    times = []
-    for i in range(6):
-        if i == 0:
-            start_time = time.time()
-            loader = dc.data.CSVLoader(["task1"], feature_field="smiles",
-                                    id_field="ids", featurizer=dc.feat.CoulombMatrix(max_atoms=70))
-            data = loader.create_dataset("Datasets/dataset_500.csv")
-        if i == 1:
-            start_time = time.time()
-            loader = dc.data.CSVLoader(["task1"], feature_field="smiles",
-                                    id_field="ids", featurizer=dc.feat.CoulombMatrix(max_atoms=70))
-            data = loader.create_dataset("Datasets/dataset_1000.csv")
-        if i == 2:
-            start_time = time.time()
-            loader = dc.data.CSVLoader(["task1"], feature_field="smiles",
-                                    id_field="ids", featurizer=dc.feat.CoulombMatrix(max_atoms=70))
-            data = loader.create_dataset("Datasets/dataset_5000.csv")
-        if i == 3:
-            start_time = time.time()
-            loader = dc.data.CSVLoader(["task1"], feature_field="smiles",
-                                    id_field="ids", featurizer=dc.feat.CoulombMatrix(max_atoms=70))
-            data = loader.create_dataset("Datasets/dataset_7000.csv")
-        if i == 4:
-            start_time = time.time()
-            loader = dc.data.CSVLoader(["task1"], feature_field="smiles",
-                                    id_field="ids", featurizer=dc.feat.CoulombMatrix(max_atoms=70))
-            data = loader.create_dataset("Datasets/dataset_10000.csv")
-        if i == 5:
-            start_time = time.time()
-            loader = dc.data.CSVLoader(["task1"], feature_field="smiles",
-                                    id_field="ids", featurizer=dc.feat.CoulombMatrix(max_atoms=70))
-            data = loader.create_dataset("Datasets/dataset_15000.csv")
+    loader = dc.data.CSVLoader(["task1", "task2", "task3"], feature_field="smiles", id_field="ids", featurizer=dc.feat.CoulombMatrix(max_atoms=70))
+    data = loader.create_dataset("Datasets/dataset_3task_1000.csv")
+    transformer = dc.trans.NormalizationTransformer(
+        dataset=data, transform_y=True)
+    dataset = transformer.transform(data)
 
-        transformer = dc.trans.NormalizationTransformer(
-            dataset=data, transform_y=True)
-        dataset = transformer.transform(data)
+    # Splits dataset into train/validation/test
+    splitter = dc.splits.RandomSplitter()
+    train_dataset, valid_dataset, test_dataset = splitter.train_valid_test_split(dataset=dataset, seed=dataseed)
+    task_count = len(train_dataset.y[0])
 
-        # Splits dataset into train/validation/test
-        splitter = dc.splits.RandomSplitter()
-        train_dataset, valid_dataset, test_dataset = splitter.train_valid_test_split(
-            dataset=dataset, seed=dataseed)
-        task_count = len(train_dataset.y[0])
+    metric = dc.metrics.Metric(dc.metrics.mean_absolute_error)
+    model = fixed_param_model(task_count=task_count)
+    loss_list = train_loss_over_epoch(model, train_dataset=train_dataset, valid_dataset=valid_dataset, metric=metric, transformer=transformer)
 
-        metrics = [
-            dc.metrics.Metric(dc.metrics.mean_absolute_error)
-            # dc.metrics.Metric(dc.metrics.r2_score)
-        ]
-        metric = dc.metrics.Metric(dc.metrics.mean_absolute_error)
-        model = fixed_param_model(task_count=task_count)
-        loss_list = train_loss_over_epoch(model, train_dataset=train_dataset, valid_dataset=valid_dataset, metric=metric, transformer=transformer)
-        end_time = time.time()
-        time_elapsed = end_time-start_time
-        times.append(time_elapsed)
-
-    df = pd.DataFrame(list(zip(times)),
-                    columns=["times(sec)"])
-    df.to_csv("dtnn_time_complexity.csv")
+    df = pd.DataFrame(list(zip(loss_list[0], loss_list[1])),columns=["training_loss", "valid_loss"])
+    df.to_csv("dtnn_3task_metric.csv")
 
 def hyperparameter_optimization(train_dataset, valid_dataset, transformer, metric):
     # parameter optimization
@@ -87,7 +48,7 @@ def hyperparameter_optimization(train_dataset, valid_dataset, transformer, metri
         'n_tasks': [task_count],
         'n_embedding': [5, 10, 50, 100],
         'dropouts': [0.2, 0.5],
-        'learning_rate': [0.001, 0.0001]
+        'learning_rate': [0.001, 0.0001, 0.00001]
     }
 
     optimizer = dc.hyper.GridHyperparamOpt(dc.models.DTNNModel)
@@ -119,15 +80,19 @@ def train_loss_over_epoch(model, train_dataset, valid_dataset, metric, transform
     for i in range(250):
         loss = model.fit(train_dataset, nb_epoch=1)
         valid = model.evaluate(valid_dataset, metric, [
-                            transformer], per_task_metrics=False)
+                            transformer], per_task_metrics=True)
         train = model.evaluate(train_dataset, metric, [
-                            transformer], per_task_metrics=False)
-        print("loss: %s" % str(loss))
+                            transformer], per_task_metrics=True)
+        print(type(valid))
+        print(valid)
+        print("\n")
+        #print("loss: %s" % str(loss))
         # train_mean.append(train[0])
         # train_task1.append(train)
         train_losses.append(train)
         valid_losses.append(valid)
     all_losses.append(train_losses)
+    print("train loss length: " + len(train_losses))
     all_losses.append(valid_losses)
     return all_losses
     
