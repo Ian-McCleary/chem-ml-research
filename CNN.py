96fb319f31369b28fc4c056e4f3e496c94a73e94
mccleai <mccleai@wwu.edu>
2021-07-29 13:29:52 -0700

    r2 testing

--- CNN.py
+++ CNN.py
@@ -127,7 +127,7 @@ def cnn_fixed_param_model(n_tasks, n_features):
         n_tasks,
         n_features,
         dims=3,
-        layer_filters=[1000],
+        layer_filters=[100, 100, 100, 100],
         kernel_size=[1, 3, 3],
         weight_init_stddevs=0.02,
         bias_init_consts=1,
