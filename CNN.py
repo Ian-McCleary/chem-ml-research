246e1d81838fb901ed92dd6b5f694e165b129a6f
ian <mccleai@wwu.edu>
2021-08-04 09:57:01 -0700

    testing

--- CNN.py
+++ CNN.py
@@ -61,7 +61,7 @@ def cnn_start_training():
     train_dataset, valid_dataset, test_dataset = splitter.train_valid_test_split(
         dataset=dataset, frac_train=0.85, frac_valid=0.15, frac_test=0.00, seed=dataseed)
     task_count = len(dataset.y[0])
-    n_features = 2*(max_a * max_a)
+    n_features = (2, 70, 70)
 
     metric = dc.metrics.Metric(dc.metrics.r2_score)
     model = cnn_fixed_param_model(task_count, n_features)
