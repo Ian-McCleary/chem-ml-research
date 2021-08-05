68178cde5270348a302f28256975039af9e6a173
mccleai <mccleai@wwu.edu>
2021-08-05 13:07:29 -0700

    finalizing testing

--- ho_test.py
+++ ho_test.py
@@ -6,4 +6,4 @@ smile = "COc1cc(C#N)c(\\N=N/c2ccccc2C(=O)O)c(C(=O)O)c1"
 m = Chem.MolFromSmiles(smile)
 atoms = m.GetAtoms()
 for i in range(len(atoms)):
-    print(atoms[i].getNumImplicitHs())
+    print(Chem.getNumImplicitHs(atoms[i]))
