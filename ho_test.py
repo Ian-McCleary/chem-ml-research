9d5630d7ee0c1885f3aca7e279c7da5081481d76
mccleai <mccleai@wwu.edu>
2021-08-05 14:39:00 -0700

    finalizing testing

--- ho_test.py
+++ ho_test.py
@@ -1,7 +1,5 @@
 import numpy as np
 from rdkit import Chem
-from rdkit import AllChem
-import deepchem as dc
 
 smiles = ["N#Cc1cc(F)c(F)c(\\N=N/c2c(C(=O)O)cccc2-c2ccccc2)c1", "COc1cccc(\\N=N/c2ccc(-c3ccccc3)c(C)c2OC)c1C(=O)O","COc1cccc(\\N=N/c2c(C)ccc(-c3ccccc3)c2C(=O)O)c1-c1ccccc1",
           "N#Cc1cc(C(=O)O)cc(\\N=N/c2cccc(-c3ccccc3)c2C(=O)O)c1C#N", "COc1cc(\\N=N/c2cccc(F)c2)cc(C#N)c1C#N", "COc1cc(C#N)cc(\\N=N/c2cc(-c3ccccc3)cc(C#N)c2C)c1",
