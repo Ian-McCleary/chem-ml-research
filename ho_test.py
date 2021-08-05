335041511088d51ec657827115f3b5450bb884ed
ian <mccleai@wwu.edu>
2021-08-05 12:30:20 -0700

    testing

--- ho_test.py
+++ ho_test.py
@@ -1,7 +1,7 @@
 import numpy as np
 from rdkit import Chem
 
-smile = "COc1cc(C#N)c(\N=N/c2ccccc2C(=O)O)c(C(=O)O)c1"
+smile = "COc1cc(C#N)c(\\N=N/c2ccccc2C(=O)O)c(C(=O)O)c1"
 
 m = Chem.MolFromSmiles(smile)
 atoms = m.GetAtoms()
