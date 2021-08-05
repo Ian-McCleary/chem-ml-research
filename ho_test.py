6787b7ed0f425d12f4ab9c32e0affd41fb3cdfc0
ian <mccleai@wwu.edu>
2021-08-05 12:30:58 -0700

    testing

--- ho_test.py
+++ ho_test.py
@@ -6,4 +6,6 @@ smile = "COc1cc(C#N)c(\\N=N/c2ccccc2C(=O)O)c(C(=O)O)c1"
 m = Chem.MolFromSmiles(smile)
 atoms = m.GetAtoms()
 for i in range(len(atoms)):
+    if atoms[i] == "O":
+        print("O")
     print(atoms[i])
