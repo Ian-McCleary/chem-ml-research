a8d32d87f0d2a77d38ec3d549d1a68b842fd295c
mccleai <mccleai@wwu.edu>
2021-08-05 13:23:47 -0700

    finalizing testing

--- ho_test.py
+++ ho_test.py
@@ -5,4 +5,6 @@ smile = "COc1cc(C#N)c(\\N=N/c2ccccc2C(=O)O)c(C(=O)O)c1"
 
 m = Chem.MolFromSmiles(smile)
 for atom in m.GetAtoms():
-    print(atom.GetSymbol(), atom.GetExplicitValence(), atom.GetTotalNumHs())
+    if atom.GetSymbol() == "O":
+        if atom.GetTotalNumHs() > 1:
+            print(atom.GetSymbol(), atom.GetExplicitValence(), atom.GetTotalNumHs())
