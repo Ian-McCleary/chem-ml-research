a1cd26b6f67a3c0a392af2e00bb6b7a223035173
mccleai <mccleai@wwu.edu>
2021-08-05 13:15:44 -0700

    finalizing testing

--- ho_test.py
+++ ho_test.py
@@ -4,6 +4,6 @@ from rdkit import Chem
 smile = "COc1cc(C#N)c(\\N=N/c2ccccc2C(=O)O)c(C(=O)O)c1"
 
 m = Chem.MolFromSmiles(smile)
-atoms = m.GetAtoms()
-for i in range(len(atoms)):
-    print(Chem.getNumImplicitHs(atoms[i]))
+for atom in m.getAtoms():
+    for bond in Chem.GetBonds(atom):
+        print (bond)
