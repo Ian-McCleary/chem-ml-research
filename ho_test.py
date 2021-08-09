96e52e71d834d50cda459e340ea2d2a0b4cc1c78
mccleai <mccleai@wwu.edu>
2021-08-09 16:19:45 -0700

    finalizing testing

--- ho_test.py
+++ ho_test.py
@@ -2,7 +2,7 @@ import numpy as np
 from rdkit import Chem
 from rdkit.Chem import AllChem
 import math
-
+from rdkit import RDLogger
 #smiles = ["N#Cc1cc(F)c(F)c(\\N=N/c2c(C(=O)O)cccc2-c2ccccc2)c1", "COc1cccc(\\N=N/c2ccc(-c3ccccc3)c(C)c2OC)c1C(=O)O","COc1cccc(\\N=N/c2c(C)ccc(-c3ccccc3)c2C(=O)O)c1-c1ccccc1",
 #          "N#Cc1cc(C(=O)O)cc(\\N=N/c2cccc(-c3ccccc3)c2C(=O)O)c1C#N", "COc1cc(\\N=N/c2cccc(F)c2)cc(C#N)c1C#N", "COc1cc(C#N)cc(\\N=N/c2cc(-c3ccccc3)cc(C#N)c2C)c1",
 #          "COc1c(C(=O)O)ccc(\\N=N/c2ccc(-c3ccccc3)cc2C(=O)O)c1C(=O)O", "COc1cc(\\N=N/c2c(C)cccc2OC)c(C(=O)O)c(-c2ccccc2)c1", "COc1cc(\\N=N/c2ccc(C(=O)O)c(C#N)c2C(=O)O)ccc1C(=O)O",
@@ -27,8 +27,9 @@ def find_half(bond_list, atom_list, previous, current):
                 return find_half(bond_list, atom_list, current, connecting_atom)
     print("this shouldnt print")
 
+lg = RDLogger.logger()
 
-
+lg.setLevel(RDLogger.CRITICAL)
 smiles = ["COc1cccc(\\N=N/c2ccc(-c3ccccc3)c(C)c2OC)c1C(=O)O"]
 for smile in smiles:
     print(smile)
