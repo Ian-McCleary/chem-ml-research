389bbfc28158bcf040585c7be663e817c64bc2d9
mccleai <mccleai@wwu.edu>
2021-08-09 15:09:35 -0700

    finalizing testing

--- ho_test.py
+++ ho_test.py
@@ -2,6 +2,7 @@ import numpy as np
 from rdkit import Chem
 from rdkit.Chem import AllChem
 import math
+import sys
 #smiles = ["N#Cc1cc(F)c(F)c(\\N=N/c2c(C(=O)O)cccc2-c2ccccc2)c1", "COc1cccc(\\N=N/c2ccc(-c3ccccc3)c(C)c2OC)c1C(=O)O","COc1cccc(\\N=N/c2c(C)ccc(-c3ccccc3)c2C(=O)O)c1-c1ccccc1",
 #          "N#Cc1cc(C(=O)O)cc(\\N=N/c2cccc(-c3ccccc3)c2C(=O)O)c1C#N", "COc1cc(\\N=N/c2cccc(F)c2)cc(C#N)c1C#N", "COc1cc(C#N)cc(\\N=N/c2cc(-c3ccccc3)cc(C#N)c2C)c1",
 #          "COc1c(C(=O)O)ccc(\\N=N/c2ccc(-c3ccccc3)cc2C(=O)O)c1C(=O)O", "COc1cc(\\N=N/c2c(C)cccc2OC)c(C(=O)O)c(-c2ccccc2)c1", "COc1cc(\\N=N/c2ccc(C(=O)O)c(C#N)c2C(=O)O)ccc1C(=O)O",
@@ -14,7 +15,7 @@ def find_half(bond_list, atom_list, index):
     for x in range(len(bond_list)):
         try:
             connecting_atom = Chem.rdchem.Bond.GetOtherAtomIdx(bond_list[x], index)
-            print("connecting: ",connecting_atom)
+            print("connecting: ", connecting_atom)
         except (RuntimeError):
             pass
             continue
