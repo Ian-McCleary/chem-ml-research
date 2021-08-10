da862c5d5f5071f795a0208dd9614c760495c58e
ian <mccleai@wwu.edu>
2021-08-10 16:48:26 -0700

    multi smiles testing

--- ho_test.py
+++ ho_test.py
@@ -3,10 +3,10 @@ from rdkit import Chem
 from rdkit.Chem import AllChem
 import math
 from rdkit import RDLogger
-#smiles = ["N#Cc1cc(F)c(F)c(\\N=N/c2c(C(=O)O)cccc2-c2ccccc2)c1", "COc1cccc(\\N=N/c2ccc(-c3ccccc3)c(C)c2OC)c1C(=O)O","COc1cccc(\\N=N/c2c(C)ccc(-c3ccccc3)c2C(=O)O)c1-c1ccccc1",
-#          "N#Cc1cc(C(=O)O)cc(\\N=N/c2cccc(-c3ccccc3)c2C(=O)O)c1C#N", "COc1cc(\\N=N/c2cccc(F)c2)cc(C#N)c1C#N", "COc1cc(C#N)cc(\\N=N/c2cc(-c3ccccc3)cc(C#N)c2C)c1",
-#          "COc1c(C(=O)O)ccc(\\N=N/c2ccc(-c3ccccc3)cc2C(=O)O)c1C(=O)O", "COc1cc(\\N=N/c2c(C)cccc2OC)c(C(=O)O)c(-c2ccccc2)c1", "COc1cc(\\N=N/c2ccc(C(=O)O)c(C#N)c2C(=O)O)ccc1C(=O)O",
-#          "O=C(O)c1ccc(-c2ccccc2)c(\\N=N/c2ccc(-c3ccccc3)c(C(=O)O)c2C(=O)O)c1"]
+smiles = ["N#Cc1cc(F)c(F)c(\\N=N/c2c(C(=O)O)cccc2-c2ccccc2)c1", "COc1cccc(\\N=N/c2ccc(-c3ccccc3)c(C)c2OC)c1C(=O)O","COc1cccc(\\N=N/c2c(C)ccc(-c3ccccc3)c2C(=O)O)c1-c1ccccc1",
+          "N#Cc1cc(C(=O)O)cc(\\N=N/c2cccc(-c3ccccc3)c2C(=O)O)c1C#N", "COc1cc(\\N=N/c2cccc(F)c2)cc(C#N)c1C#N", "COc1cc(C#N)cc(\\N=N/c2cc(-c3ccccc3)cc(C#N)c2C)c1",
+          "COc1c(C(=O)O)ccc(\\N=N/c2ccc(-c3ccccc3)cc2C(=O)O)c1C(=O)O", "COc1cc(\\N=N/c2c(C)cccc2OC)c(C(=O)O)c(-c2ccccc2)c1", "COc1cc(\\N=N/c2ccc(C(=O)O)c(C#N)c2C(=O)O)ccc1C(=O)O",
+          "O=C(O)c1ccc(-c2ccccc2)c(\\N=N/c2ccc(-c3ccccc3)c(C(=O)O)c2C(=O)O)c1"]
 #TODO Implement backtracking instead of recursion. Check each possible route to the nearest N
 
 
@@ -44,7 +44,7 @@ def find_nearest_oxygen_or_carbon(current, previous, bond_list, atom_list):
 lg = RDLogger.logger()
 
 lg.setLevel(RDLogger.CRITICAL)
-smiles = ["COc1cccc(\\N=N/c2ccc(-c3ccccc3)c(C)c2OC)c1C(=O)O"]
+#smiles = ["COc1cccc(\\N=N/c2ccc(-c3ccccc3)c(C)c2OC)c1C(=O)O"]
 for smile in smiles:
     print(smile)
     m = Chem.MolFromSmiles(smile)
@@ -71,8 +71,8 @@ for smile in smiles:
         if a_1.GetSymbol() == "O":
 
             oxy_count+=1
-            print("\n")
-            print("Oxygen Number: " + str(oxy_count))
+            #print("\n")
+            #print("Oxygen Number: " + str(oxy_count))
             num_bonded_hydrogens = 0
             h_half = False
             bonded_h = False
@@ -89,13 +89,13 @@ for smile in smiles:
                     else:
                         potential_cov = False
 
-                    print("o_half: ",o_half,"  h_half: ",h_half)
+                    #print("o_half: ",o_half,"  h_half: ",h_half)
 
                     oxy_pos = pos[i]
                     hydro_pos = pos[j]
                     distance = math.sqrt((oxy_pos[0]-hydro_pos[0])**2 + (oxy_pos[1]-hydro_pos[1])**2 +
                                          (oxy_pos[2]-hydro_pos[2])**2)
-                    print(distance)
+                    #print(distance)
                     if distance < 1.5 and potential_cov is True:
                         bonded_h = True
                         bonded_h_val = distance
@@ -117,6 +117,7 @@ for smile in smiles:
                                     failed = True
                         if failed == True:
                             print("Failed")
+                            break
                         else:
                             print("Passed")
 
