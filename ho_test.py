5857bd14a823516fea62c07d66ef7e0dffcda252
mccleai <mccleai@wwu.edu>
2021-08-12 14:57:22 -0700

    dataset bug?

--- ho_test.py
+++ ho_test.py
@@ -107,7 +107,7 @@ lg = RDLogger.logger()
 
 lg.setLevel(RDLogger.CRITICAL)
 #smiles = ["COc1cccc(\\N=N/c2ccc(-c3ccccc3)c(C)c2OC)c1C(=O)O"]
-for smile in passed_smiles:
+for smile in smiles:
     print(smile)
     m = Chem.MolFromSmiles(smile)
     m = Chem.AddHs(m)
