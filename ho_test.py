f7ef990e92d5181b87d911e727aa38d5adab7453
mccleai <mccleai@wwu.edu>
2021-08-12 15:08:01 -0700

    dataset bug?

--- ho_test.py
+++ ho_test.py
@@ -10,22 +10,6 @@ passed_smiles = ["COc1cc(\\N=N/c2c(C)ccc(-c3ccccc3)c2C#N)ccc1C(=O)O", "Cc1ccc(\\
                  "N#Cc1ccccc1\\N=N/c1cc(C(=O)O)ccc1-c1ccccc1", "O=C(O)c1cc(F)c(\\N=N/c2cccc(F)c2)c(C(=O)O)c1", "COc1ccc(OC)c(\\N=N/c2c(F)ccc(-c3ccccc3)c2C#N)c1"]
 
 
-# false = first half, true = second half
-def find_half(atom_list, bond_list, start, tracking_list):
-    visited_list = []
-    path = find_nearest_oxygen_or_carbon(atom_list, bond_list, start, tracking_list)
-    i = path
-    while i < len(atom_list):
-        a_1 = i
-        if i < len(atom_list)-1:
-            a_2 = i+1
-
-        if atom_list[a_1].GetSymbol() == "N" and atom_list[a_2].GetSymbol() == "N":
-            return False
-        i+=1
-    return True
-
-
 def find_nearest_oxygen_or_carbon(atom_list, bond_list, start, tracking_list):
     tracking_list.append(start)
     for i in range(len(bond_list)):
@@ -81,7 +65,7 @@ def backtracking_find_half(atom_list, bond_list, start, tracking_list):
                     return return_val
         return -1
 
-
+# false = first half, true = second half
 def find_half2(atom_list, bond_list, start, tracking_list):
     n_pos = backtracking_find_half(atom_list, bond_list, start, tracking_list)
     if atom_list[n_pos].GetSymbol() == "N" and atom_list[n_pos+1].GetSymbol() == "N":
@@ -102,64 +86,75 @@ def nearest_oxygen_distance(atom_list, pos_array, oxy_pos):
     return min_distance
 
 
+def start_filtering():
+    lg = RDLogger.logger()
 
-lg = RDLogger.logger()
+    lg.setLevel(RDLogger.CRITICAL)
+    # smiles = ["COc1cccc(\\N=N/c2ccc(-c3ccccc3)c(C)c2OC)c1C(=O)O"]
+    for smile in smiles:
+        print(smile)
+        m = Chem.MolFromSmiles(smile)
+        m = Chem.AddHs(m)
+        status = AllChem.EmbedMolecule(m)
+        conformer = m.GetConformer()
+        pos = conformer.GetPositions()
+        oxy_count = 0
 
-lg.setLevel(RDLogger.CRITICAL)
-#smiles = ["COc1cccc(\\N=N/c2ccc(-c3ccccc3)c(C)c2OC)c1C(=O)O"]
-for smile in smiles:
-    print(smile)
-    m = Chem.MolFromSmiles(smile)
-    m = Chem.AddHs(m)
-    status = AllChem.EmbedMolecule(m)
-    conformer = m.GetConformer()
-    pos = conformer.GetPositions()
-    oxy_count = 0
+        atom_list = m.GetAtoms()
+        bond_list = m.GetBonds()
+        for i in range(len(atom_list)):
+            # check which half oxygen is on
+            # print(atom_list[i].GetSymbol())
+            if atom_list[i].GetSymbol() == "O":
+                track = []
+                o_half = find_half2(atom_list, bond_list, i, track)
+                tracking_list = []
+                oxy_count += 1
+                # print("\n")
+                # print("Oxygen Number: " + str(oxy_count))
+                has_covalent_bond = has_covalent_hydrogen_bond(i, atom_list, bond_list)
+                bonded_h_val = 0
+                for j in range(len(atom_list)):
+                    b_1 = atom_list[j]
+                    if b_1.GetSymbol() == "H":
 
-    atom_list = m.GetAtoms()
-    bond_list = m.GetBonds()
-    for i in range(len(atom_list)):
-        #check which half oxygen is on
-        #print(atom_list[i].GetSymbol())
-        if atom_list[i].GetSymbol() == "O":
-            track = []
-            o_half = find_half2(atom_list, bond_list, i, track)
-            tracking_list = []
-            oxy_count+=1
-            #print("\n")
-            #print("Oxygen Number: " + str(oxy_count))
-            has_covalent_bond = has_covalent_hydrogen_bond(i, atom_list, bond_list)
-            bonded_h_val = 0
-            for j in range(len(atom_list)):
-                b_1 = atom_list[j]
-                if b_1.GetSymbol() == "H":
+                        # recursively check the side of each hydrogen atom
+                        track_list = []
+                        near_o_c = find_nearest_oxygen_or_carbon(atom_list, bond_list, j, track_list)
+                        print("tracklist: ",track_list)
+                        track_list = []
+                        h_half = find_half2(atom_list, bond_list, near_o_c, track_list)
+                        # print("answer: ", answer)
+                        # print(o_half, h_half)
+                        if (not o_half is True and h_half is True) or (not o_half is False and h_half is False):
+                            failed = False
+                            oxy_pos = pos[i]
+                            hydro_pos = pos[j]
+                            hydrogen_distance = math.sqrt(
+                                (oxy_pos[0] - hydro_pos[0]) ** 2 + (oxy_pos[1] - hydro_pos[1]) ** 2 +
+                                (oxy_pos[2] - hydro_pos[2]) ** 2)
+                            # print(distance)
+                            if hydrogen_distance < 4.3 and has_covalent_bond is True:
+                                failed = True
+                                # nearby_o = nearest_oxygen_distance(atom_list, pos, oxy_pos)
+                                # if nearby_o < hydrogen_distance and nearby_o < 2.3:
+                                #    failed = False
+                                # else:
+                                #    failed = True
+                            if failed == True:
+                                print("Failed: ", oxy_count, "  ", hydrogen_distance)
+                                break
+    #                        else:
+    # t                            print("Passed")
 
-                    #recursively check the side of each hydrogen atom
-                    track_list = []
-                    near_o_c = find_nearest_oxygen_or_carbon(atom_list, bond_list, j, track_list)
-                    track_list = []
-                    h_half = find_half2(atom_list, bond_list, near_o_c, track_list)
-                    #print("answer: ", answer)
-                    #print(o_half, h_half)
-                    if (not o_half is True and h_half is True) or (not o_half is False and h_half is False):
-                        failed = False
-                        oxy_pos = pos[i]
-                        hydro_pos = pos[j]
-                        hydrogen_distance = math.sqrt((oxy_pos[0]-hydro_pos[0])**2 + (oxy_pos[1]-hydro_pos[1])**2 +
-                                            (oxy_pos[2]-hydro_pos[2])**2)
-                        #print(distance)
-                        if hydrogen_distance < 4.3 and has_covalent_bond is True:
-                            failed = True
-                            #nearby_o = nearest_oxygen_distance(atom_list, pos, oxy_pos)
-                            #if nearby_o < hydrogen_distance and nearby_o < 2.3:
-                            #    failed = False
-                            #else:
-                            #    failed = True
-                        if failed == True:
-                            print("Failed: ", oxy_count,"  ", hydrogen_distance)
-                            break
-#                        else:
-#t                            print("Passed")
+
+def main():
+    # args = parse_args()
+    start_filtering()
+
+
+if __name__ == "__main__":
+    main()
 
 
 
