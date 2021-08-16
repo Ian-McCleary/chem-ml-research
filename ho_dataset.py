5689379103fcd8d1edff43183bfd0ec5e36b99af
ian <mccleai@wwu.edu>
2021-08-16 10:51:45 -0700

    testing

--- ho_dataset.py
+++ ho_dataset.py
@@ -4,14 +4,11 @@ from rdkit import Chem
 from rdkit.Chem import AllChem
 import math
 from rdkit import RDLogger
-import deepchem as dc
-import numpy as np
 import pandas as pd
-import tensorflow as tf
 import csv
 
 with open("failed_molecules.csv", newline='') as csvfile:
-    spamreade = csv.reader(csvfile, delimiter= ' ', quotechar='|')
+    spamreader = csv.reader(csvfile, delimiter= ' ', quotechar='|')
     for row in spamreader:
         print(row[3])
 
