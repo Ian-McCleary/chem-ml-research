955d87c9c1e194dee84c726039785fbc1ae9dfe4
ian <mccleai@wwu.edu>
2021-08-17 10:58:38 -0700

    hydrogen position testing

--- add_h.py
+++ add_h.py
@@ -6,3 +6,29 @@ import math
 from rdkit import RDLogger
 import pandas as pd
 import csv
+
+for x in range(3):
+    print("Batch:",x,"\n")
+    with open("failed_molecules.csv", newline='') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter= ',', quotechar='|')
+            mol_count = 0
+            for row in spamreader:
+                if mol_count == 0:
+                    mol_count+=1
+                    continue
+                smile = row[3]
+                m = Chem.MolFromSmiles(smile)
+                m = Chem.AddHs(m)
+                status = AllChem.EmbedMolecule(m)
+                conformer = m.GetConformer()
+                pos = conformer.GetPositions()
+
+                atom_list = m.GetAtoms()
+                for i in range(len(atom_list)):
+                    if atom_list[i].GetSymbol() == "H":
+                        h_pos = pos[i]
+                        print(i, "x:",h_pos[0],"y:",h_pos[1],"z:",h_pos[2])
+                mol_count+=1
+                if mol_count >4:
+                    break
+        
