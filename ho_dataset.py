0614643570308c496b1b529bd1d45b9c57e3004d
ian <mccleai@wwu.edu>
2021-08-19 12:52:28 -0700

    testing

--- ho_dataset.py
+++ ho_dataset.py
@@ -54,9 +54,9 @@ def get_mol_from_xyz(mol_id):
                             stop = True
                     else:
                         break
-    #for i in range(len(reversed_line_arr)):
-    #    print(reversed_line_arr[len(reversed_line_arr)-i-1])
-    print(reversed_line_arr)
+    for i in range(len(reversed_line_arr)):
+        print(reversed_line_arr[len(reversed_line_arr)-i-1])
+    #print(reversed_line_arr)
 
 
 
