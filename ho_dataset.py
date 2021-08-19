9593a1d58a9cae2690f5b5fa424d4893c938dba8
ian <mccleai@wwu.edu>
2021-08-19 13:25:31 -0700

    testing

--- ho_dataset.py
+++ ho_dataset.py
@@ -55,7 +55,7 @@ def get_mol_from_xyz(mol_id):
                     else:
                         break
     file_name = str(mol_id) + ".xyz"
-    file_path = os.path.join("/xyz", file_name)
+    file_path = os.path.join("xyz", file_name)
     file = open(file_path, "x")
     for i in range(len(reversed_line_arr)):
         print(reversed_line_arr[len(reversed_line_arr)-i-1])
